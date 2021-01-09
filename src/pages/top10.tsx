690ad5bdd609f9b6b71f16c8e4b1940642975a07
João Paulo Fricks <jpfricks@gmail.com>
2021-01-08 22:45:25 -0300

    chore(fake-api): create fake-api 🥁
    * create json file
    * create api variable
    * configure .env.local
    * client side fetching
    * server side fetching
    * static site generation

---
+++ src/pages/top10.tsx
@@ -0,0 +1,45 @@
+import { GetStaticProps } from 'next'
+import { Container } from '../styles/pages/top10'
+
+interface IProduct {
+  id: string;
+  title: string;
+}
+
+interface Top10Props {
+  products: IProduct[]
+}
+
+export default function TopProducts({products }:Top10Props) {
+  return (
+    <Container>
+      <section>
+        <h1>Top 10</h1>
+        <ul>
+          {products.map(product => {
+            return (
+              <li key={product.id}>
+                <p>{product.title}</p>
+              </li>
+            )
+          })}
+        </ul>
+
+      </section>
+    </Container>
+  )
+}
+
+// TODO: STATIC SITE GENERATION
+export const getStaticProps: GetStaticProps<Top10Props> = async (context) => {
+  const apiUrl = process.env.API_URL + '/products';
+  const response = await fetch(apiUrl);
+  const products = await response.json();
+
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60
+  }
+}
