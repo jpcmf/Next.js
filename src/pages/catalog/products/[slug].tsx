d73a3cbb4c0c85161ed1cf0761ad9c559deda421
João Paulo Fricks <jpfricks@gmail.com>
2021-01-17 18:40:48 -0300

    feat(lazy): add example of lazy load components

--- src/pages/catalog/products/[slug].tsx
+++ src/pages/catalog/products/[slug].tsx
@@ -1,8 +1,32 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import { useCallback, useState } from "react";
+import { Container } from "../../../styles/pages/home";
+
+const AddToCartModal = dynamic(
+  () => import("../../../components/AddToCartModal"),
+  {
+    loading: () => <p>Loading...</p>,
+    ssr: false, //TODO: ssr false enable component load in the client-side (browser)
+  }
+);
 
 export default function Product() {
   const router = useRouter();
 
-  return <h1>{router.query.slug}</h1>
-}
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false);
 
+  const handleAddToCart = useCallback(() => {
+    setIsAddToCartModalVisible(true);
+  }, []);
+
+  return (
+    <Container>
+      <h1>{router.query.slug}</h1>
+
+      <button onClick={handleAddToCart}>Add to cart</button>
+
+      {isAddToCartModalVisible && <AddToCartModal />}
+    </Container>
+  );
+}
